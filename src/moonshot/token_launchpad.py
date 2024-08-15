e3d696a5b8523a0563caa179287dc47c290a2fcd
Rui Qi Chen <ruiqic@gatech.edu>
2024-08-15 12:16:31 -0400

    tx sender

--- src/moonshot/token_launchpad.py
+++ src/moonshot/token_launchpad.py
@@ -1,32 +1,28 @@
-import json
-import os
-import anchorpy
 from solders.pubkey import Pubkey
-from solders.keypair import Keypair
-from solders.transaction import TransactionVersion, Legacy
+from solders.transaction import VersionedTransaction
 from solders.instruction import Instruction
-from solders.system_program import ID
-from solders.sysvar import RENT
+from solders.message import MessageV0
+from solders.hash import Hash
 from solders.signature import Signature
-from solders.address_lookup_table_account import AddressLookupTableAccount
+from solders.rpc.responses import SendTransactionResp
 from solana.rpc.async_api import AsyncClient
 from solana.rpc.types import TxOpts
-from solana.rpc.commitment import Processed
-from solana.transaction import AccountMeta
+from solana.rpc.commitment import Processed, Confirmed
 from solders.compute_budget import set_compute_unit_limit, set_compute_unit_price
 from spl.token.constants import TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID
 from solders.system_program import ID as SYS_PROGRAM_ID
 from spl.token.instructions import get_associated_token_address
 from anchorpy import Program, Context, Idl, Provider, Wallet
 from pathlib import Path
+from typing import Optional
 
 import moonshot
 from moonshot.constants import MOONSHOT_PROGRAM_ID, HELIO_FEE_ID, DEX_FEE_ID, CONFIG_ACCOUNT_ID
-from moonshot.types import is_variant, CurveAccount, CurveType, TradeType, FixedSide, TradeParams
-from moonshot.curve import ConstantProductCurveV1, LinearCurveV1
+from moonshot.types import is_variant, CurveAccount, TradeType, FixedSide, TradeParams
+from moonshot.curve import AbstractCurve, ConstantProductCurveV1, LinearCurveV1
 from moonshot.get_accounts import get_curve_account
 
-DEFAULT_TX_OPTIONS = TxOpts(skip_confirmation=False, preflight_commitment=Processed)
+DEFAULT_TX_OPTIONS = TxOpts(skip_confirmation=True, skip_preflight=False, preflight_commitment=Processed)
 DEFAULT_FIXED_SIDE = FixedSide.ExactIn()
 
 class TokenLaunchpad:
@@ -41,6 +37,7 @@ class TokenLaunchpad:
         self.wallet = wallet
         self.authority = wallet.public_key
         self.token_mint = token_mint
+        self.opts = opts
 
         file = Path(str(moonshot.__path__[0]) + "/moonshot.json")
         with file.open() as f:
@@ -68,7 +65,7 @@ class TokenLaunchpad:
         self,
         amount: int,
         trade_direction: TradeType
-    ):
+    ) -> int:
         curve_account = await self.get_curve_account()
         if self.curve is None:
             self.curve = self.get_curve(curve_account)
@@ -78,16 +75,16 @@ class TokenLaunchpad:
         self,
         amount: int,
         trade_direction: TradeType
-    ):
+    ) -> int:
         curve_account = await self.get_curve_account()
         if self.curve is None:
             self.curve = self.get_curve(curve_account)
         return self.curve.get_collateral_amount_from_tokens(amount, curve_account, trade_direction)
 
-    async def get_curve_account(self):
+    async def get_curve_account(self) -> CurveAccount:
         return await get_curve_account(self.program, self.curve_account_pubkey)
     
-    def get_curve(self, curve_account: CurveAccount):
+    def get_curve(self, curve_account: CurveAccount) -> AbstractCurve:
         if is_variant(curve_account.curve_type, "ConstantProductV1"):
             return ConstantProductCurveV1()
         elif is_variant(curve_account.curve_type, "LinearV1"):
@@ -98,9 +95,9 @@ class TokenLaunchpad:
     async def get_buy_ix(
         self,
         amount: int,
-        fixed_side: FixedSide = None,
+        fixed_side: Optional[FixedSide] = None,
         slippage_bps: int = 100
-    ):
+    ) -> Instruction:
         if fixed_side is None:
             fixed_side = DEFAULT_FIXED_SIDE
 
@@ -141,9 +138,9 @@ class TokenLaunchpad:
     async def get_sell_ix(
         self,
         amount: int,
-        fixed_side: FixedSide = None,
+        fixed_side: Optional[FixedSide] = None,
         slippage_bps: int = 100
-    ):
+    ) -> Instruction:
         if fixed_side is None:
             fixed_side = DEFAULT_FIXED_SIDE
 
@@ -180,8 +177,32 @@ class TokenLaunchpad:
             ),
         )
         return ix
-    
-    
+
+    async def fetch_latest_blockhash(self) -> Hash:
+        return (
+            await self.connection.get_latest_blockhash(Confirmed)
+        ).value.blockhash
+
+    async def send_ix(
+        self,
+        ix : Instruction,
+        compute_unit_price: int = 20_000,
+        compute_unit_limit: int = 100_000,
+    ) -> Signature:
+        compute_price_ix = set_compute_unit_price(compute_unit_price)
+        compute_limit_ix = set_compute_unit_limit(compute_unit_limit)
+        ixs = [compute_limit_ix, compute_price_ix, ix]
+
+        latest_blockhash = await self.fetch_latest_blockhash()
+        msg = MessageV0.try_compile(
+            self.authority, ixs, [], latest_blockhash
+        )
+        tx = VersionedTransaction(msg, [self.wallet.payer])
+        body = self.connection._send_raw_transaction_body(bytes(tx), self.opts)
+        resp = await self.connection._provider.make_request(body, SendTransactionResp)
+        return resp.value
+
+
 
 
 
